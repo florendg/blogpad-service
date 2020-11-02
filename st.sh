577be53a7ea4bd7a1fc3c59026b279222200203f
floren <florendg@gmail.com>
2020-11-02 07:36:37 +0100

    Configurable system tests

---
+++ st.sh
@@ -0,0 +1,4 @@
+#1/bin/sh
+
+mvn clean test-compile failsafe:integration-test failsafe:verify -f content/content-st/pom.xml
+mvn clean test-compile failsafe:integration-test failsafe:verify -f reactor/reactor-st/pom.xml
