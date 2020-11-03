4f100daf90b1d69700fb74036efac04067cb4df8
floren <florendg@gmail.com>
2020-11-03 21:09:17 +0100

    Implemented templated parsing

--- reactor/reactor/src/main/java/com/weirdduke/reactor/posts/control/Renderer.java
+++ reactor/reactor/src/main/java/com/weirdduke/reactor/posts/control/Renderer.java
@@ -1,14 +1,48 @@
 package com.weirdduke.reactor.posts.control;
 
+import org.eclipse.microprofile.openapi.models.security.SecurityScheme;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Renderer {
 
-    String render(String template, String content) {
-        try (Context jsContext = Context.create("js")) {
-            var bindings = jsContext.getBindings("js");
-            bindings.putMember("message", "Duke");
-            return jsContext.eval("js", "message + 42").toString();
+    private Source source;
+
+    @PostConstruct
+    public void init() {
+        try {
+            source = Source.newBuilder("js", loadHandleBars(), "HandleBars").build();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Could not load handleBars", ioe);
         }
+    }
+
+    String render(String templateContent, String postContent) {
+        try (Context jsContext = Context.create("js")) {
+            jsContext.eval(source);
+            var bindings = jsContext.getBindings("js");
+            bindings.putMember("templateContent", templateContent);
+            bindings.putMember("postContent", postContent);
+            return jsContext.eval("js", getRenderLogic()).toString();
+        }
+    }
+
+    private String getRenderLogic() {
+        //language=JavaScript
+        return """
+                const postAsJson = JSON.parse(postContent)
+                const compiledTemplate = Handlebars.compile(templateContent);
+                compiledTemplate(postAsJson)
+                """;
+    }
+
+    private Reader loadHandleBars() {
+        var stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("js/handlebars-v4.7.6.js");
+        return new InputStreamReader(stream);
     }
 }
--- reactor/reactor/src/test/java/com/weirdduke/reactor/posts/control/ReactorTest.java
+++ reactor/reactor/src/test/java/com/weirdduke/reactor/posts/control/ReactorTest.java
@@ -1,5 +1,6 @@
 package com.weirdduke.reactor.posts.control;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,8 +8,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReactorTest {
 
+    private Renderer cut;
+
+    @BeforeEach
+    void setup() {
+        cut = new Renderer();
+        cut.init();
+    }
+
     @Test
     void render() {
-        assertEquals("Duke42", new Renderer().render("", ""));
+
+        assertEquals(
+                //language=html
+                """
+                        <html>
+                            <head>
+                                <title>Hello Duke</title>
+                            </head>
+                            <body>
+                                <h1>Hello Duke</h1>
+                                <p>Welcome to the pleasure dome</p>
+                            </body>
+                        </html>
+                        """, cut.render(
+                        //language=html
+                        """
+                                <html>
+                                    <head>
+                                        <title>{{title}}</title>
+                                    </head>
+                                    <body>
+                                        <h1>{{title}}</h1>
+                                        <p>{{content}}</p>
+                                    </body>
+                                </html>
+                                """,
+                        //language=JSON
+                        """
+                                {"title":"Hello Duke",
+                                "content":"Welcome to the pleasure dome"}
+                                """));
     }
 }
